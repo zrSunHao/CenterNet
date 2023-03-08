5d875a50dcc96149e9e2c1280eaac9e2f87f71ba
sunhao <2080680175@qq.com>
2023-03-08 19:10:45 +0800

    test updated!

--- test.py
+++ test.py
@@ -30,15 +30,19 @@ if os.path.exists(model_path):
 model_ft.to(device)
 
 transform = Augmentation()
-for index,file in enumerate(os.listdir(cfg.test_img_dir)):
-    path = cfg.test_img_dir+'/'+file
+for index, file in enumerate(os.listdir(cfg.test_img_dir)):
+    path = cfg.test_img_dir + '/' + file
     img = cv2.imread(path, cv2.IMREAD_COLOR)
+    img = transform(img, boxes=None, labels=None)
 
-    x = t.from_numpy(transform(img,boxes=None,labels=None)[0][:,:,(2,1,0)]).permute(2,0,1)
+    x = t.from_numpy(img[0][:,:,(2,1,0)]).permute(2,0,1)
     x = x.unsqueeze(0).to(device)
 
-    bbox_pred,score,cls_ind = model_ft.generate(x)
-    bbox_pred = bbox_pred * np.array([[img.shape[1],img.shape[0],img.shape[1],img.shape[0]]])
+    bbox_pred, score, cls_ind = model_ft.generate(x)
+    bbox_pred = bbox_pred * np.array([[img.shape[1],
+                                       img.shape[0],
+                                       img.shape[1],
+                                       img.shape[0]]])
     
     for i,box in enumerate(bbox_pred):
         if score[i]>0.35:
@@ -46,12 +50,29 @@ for index,file in enumerate(os.listdir(cfg.test_img_dir)):
             cls_id = class_index[int(cls_indx)]
             cls_name = class_labels[cls_id]
             label = '%s:%.3f' % (cls_name,score[i])
-            xmin,ymin,xmax,ymax = box
+            xmin, ymin, xmax, ymax = box
 
-            cv2.rectangle(img,(int(xmin),int(ymin)),(int(xmax),int(ymax)),class_color[int(cls_indx)],2)
-            cv2.rectangle(img,(int(xmin),int(abs(ymin)-15)),(int(xmin+int(xmax-xmin)*0.55),int(ymin)),class_color[int(cls_indx)],-1)
-            cv2.putText(img,label,(int(xmin),int(ymin)),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,0),2)
+            pt1 = (int(xmin), int(ymin))
+            pt2 = (int(xmax), int(ymax))
+            color = class_color[int(cls_indx)]
+            cv2.rectangle(img, pt1, pt2, color, thickness=2)
+
+            pt1 = (int(xmin), int(abs(ymin) - 15))
+            pt2 = (int(xmin + int(xmax-xmin) * 0.55), int(ymin))
+            color = class_color[int(cls_indx)]
+            # thickness为负值，表示填充整个矩形
+            cv2.rectangle(img, pt1, pt2, color, thickness=-1)
+
+            cv2.putText(img, 
+                        text = label, 
+                        org = (int(xmin),int(ymin)),
+                        fontFace = cv2.FONT_HERSHEY_SIMPLEX,
+                        fontScale = 0.5,
+                        color = (0,0,0),
+                        thickness = 2
+                        )
     
-    cv2.imwrite(os.path.join(cfg.test_out_dir,str(index).zfill(3)+'.jpg'),img)
+    img_path = os.path.join(cfg.test_out_dir, str(index).zfill(3)+'.jpg')
+    cv2.imwrite(img_path, img)
 
 
